d792e477e27e6ac3e432bfc294ad25c83d186ccc
kaggle-salaryPred <57259614+kaggle-salaryPred@users.noreply.github.com>
2019-11-14 15:12:54 +0000

    Uploading 16k score code

---
+++ predict.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  3 19:56:06 2019
+
+@author: Akash Verma
+"""
+import pandas as pd  
+import numpy as np  
+import matplotlib.pyplot as plt  
+from sklearn.model_selection import train_test_split 
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+import seaborn as sns
+import category_encoders as ce
+from sklearn.preprocessing import LabelBinarizer
+#import xgboost as xgb
+from sklearn.ensemble import RandomForestRegressor
+
+
+#To load Input data
+dataset_test = pd.read_csv('tcd-ml-1920-group-income-train.csv')
+dataset_pred = pd.read_csv('tcd-ml-1920-group-income-test.csv')
+store_data = dataset_pred.filter(['Instance'], axis=1)
+dataset_test.columns = dataset_test.columns.str.strip().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
+dataset_pred.columns = dataset_pred.columns.str.strip().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
+#print(dataset_test.shape)
+dataset_test.rename(columns={'Work_Experience_in_Current_Job_[years]': 'Work_Experience_in_Current_Job'}, inplace=True)
+dataset_pred.rename(columns={'Work_Experience_in_Current_Job_[years]': 'Work_Experience_in_Current_Job'}, inplace=True)
+#print(dataset_test.columns)
+#Remove Instance Column
+dataset_test = dataset_test.drop('Instance', 1)
+dataset_pred = dataset_pred.drop('Instance', 1)
+dataset_test.drop(dataset_test.loc[dataset_test['Total_Yearly_Income_[EUR]']==2548791].index, inplace=True)
+#Check if year is null
+#print('NUll records in year: ',dataset_test['Year_of_Record'].isnull().sum())
+#Change null to rand value between std dev around mean. Alternate could be panda interpolate
+Column_Name_avg = dataset_test['Year_of_Record'].mean()
+Column_Name_std = dataset_test['Year_of_Record'].std()
+Column_Name_null_count = dataset_test['Year_of_Record'].isnull().sum()
+Column_Name_null_random_list = np.random.randint(Column_Name_avg - Column_Name_std, Column_Name_avg + Column_Name_std, size=Column_Name_null_count)
+dataset_test['Year_of_Record'][np.isnan(dataset_test['Year_of_Record'])] = Column_Name_null_random_list
+dataset_test['Year_of_Record'] = dataset_test['Year_of_Record'].astype(int)
+
+Column_Name_avg = dataset_pred['Year_of_Record'].mean()
+Column_Name_std = dataset_pred['Year_of_Record'].std()
+Column_Name_null_count = dataset_pred['Year_of_Record'].isnull().sum()
+Column_Name_null_random_list = np.random.randint(Column_Name_avg - Column_Name_std, Column_Name_avg + Column_Name_std, size=Column_Name_null_count)
+dataset_pred['Year_of_Record'][np.isnan(dataset_pred['Year_of_Record'])] = Column_Name_null_random_list
+dataset_pred['Year_of_Record'] = dataset_pred['Year_of_Record'].astype(int)
+
+#Filling Gender Nan and other values
+dataset_test.Gender = dataset_test.Gender.replace("f", "female")
+dataset_test["Gender"].fillna("unknown", inplace = True)
+dataset_test.Gender = dataset_test.Gender.replace("0", "unknown")
+
+dataset_pred.Gender = dataset_pred.Gender.replace("f", "female")
+dataset_pred["Gender"].fillna("unknown", inplace = True)
+dataset_pred.Gender = dataset_pred.Gender.replace("0", "unknown")
+
+#Filling University Degree Nan Values
+dataset_test.University_Degree = dataset_test.University_Degree.replace("0", "No")
+dataset_test["University_Degree"].fillna("No", inplace = True)
+
+dataset_pred.University_Degree = dataset_pred.University_Degree.replace("0", "No")
+dataset_pred["University_Degree"].fillna("No", inplace = True)
+
+#Filling Haircolor Nan Values
+dataset_test.Hair_Color = dataset_test.Hair_Color.replace("0", "Unknown")
+dataset_test["Hair_Color"].fillna("Unknown", inplace = True)
+dataset_test = dataset_test.drop('Hair_Color', 1)
+
+dataset_pred.Hair_Color = dataset_pred.Hair_Color.replace("0", "Unknown")
+dataset_pred["Hair_Color"].fillna("Unknown", inplace = True)
+dataset_pred = dataset_pred.drop('Hair_Color', 1)
+
+#Filling Profession Nan Values
+dataset_test["Profession"].fillna("Unknown", inplace = True)
+dataset_pred["Profession"].fillna("Unknown", inplace = True)
+
+#Filling Housing_Situation Corrupt Values
+dataset_test["Housing_Situation"] = dataset_test["Housing_Situation"].replace(0,'Unknown')
+dataset_test["Housing_Situation"] = dataset_test["Housing_Situation"].replace('0','Unknown')
+dataset_test["Housing_Situation"] = dataset_test["Housing_Situation"].replace('nA','Unknown')
+
+dataset_pred["Housing_Situation"] = dataset_pred["Housing_Situation"].replace(0,'Unknown')
+dataset_pred["Housing_Situation"] = dataset_pred["Housing_Situation"].replace('0','Unknown')
+dataset_pred["Housing_Situation"] = dataset_pred["Housing_Situation"].replace('nA','Unknown')
+
+
+dataset_test['Satisfation_with_employer'] = dataset_test['Satisfation_with_employer'].fillna(method='ffill')
+dataset_pred['Satisfation_with_employer'] = dataset_pred['Satisfation_with_employer'].fillna(method='ffill')
+
+
+#Convert work Experience in current job to float and replace #NUM! to mean of their Age
+dataset_test.Work_Experience_in_Current_Job = dataset_test.Work_Experience_in_Current_Job.replace("#NUM!", None)
+dataset_test['Work_Experience_in_Current_Job'] = dataset_test['Work_Experience_in_Current_Job'].astype(float)
+dataset_test.value = dataset_test.groupby('Age')['Work_Experience_in_Current_Job'].apply(lambda x: x.fillna(x.median()))
+dataset_test.value = dataset_test.Work_Experience_in_Current_Job.fillna(dataset_test.Work_Experience_in_Current_Job.median())
+
+dataset_pred.Work_Experience_in_Current_Job = dataset_pred.Work_Experience_in_Current_Job.replace("#NUM!", None)
+dataset_pred['Work_Experience_in_Current_Job'] = dataset_pred['Work_Experience_in_Current_Job'].astype(float)
+dataset_pred.value = dataset_pred.groupby('Age')['Work_Experience_in_Current_Job'].apply(lambda x: x.fillna(x.median()))
+dataset_pred.value = dataset_pred.Work_Experience_in_Current_Job.fillna(dataset_pred.Work_Experience_in_Current_Job.median())
+
+#Remove EUR in Yearly_Income to make it numerical value 
+dataset_test['Yearly_Income_in_addition_to_Salary_e.g._Rental_Income'] = dataset_test['Yearly_Income_in_addition_to_Salary_e.g._Rental_Income'].replace('EUR', '', regex=True).astype(float)
+dataset_pred['Yearly_Income_in_addition_to_Salary_e.g._Rental_Income'] = dataset_pred['Yearly_Income_in_addition_to_Salary_e.g._Rental_Income'].replace('EUR', '', regex=True).astype(float)
+#dataset_test['Salary_by_Year'] = dataset_test.groupby('Year_of_Record')['Total_Yearly_Income_[EUR]'].transform('median')
+#dataset_test['Salary_by_Profession'] = dataset_test.groupby('Profession')['Total_Yearly_Income_[EUR]'].transform('mean')
+
+
+#Label encoding the categorical columns
+for col in dataset_test.dtypes[dataset_test.dtypes == 'object'].index.tolist():
+    feat_le = LabelEncoder()
+    train_list = dataset_test[col].unique()
+    dataset_test.loc[1201,col] = 'other'
+    test_list = dataset_pred[col].unique()
+    test_replace = list(set(test_list) - set(train_list))
+    dataset_pred[col] = dataset_pred[col].replace(test_replace, 'other')
+    feat_le.fit(dataset_test[col].unique().astype(str))
+    dataset_test[col] = feat_le.transform(dataset_test[col].astype(str))
+    dataset_pred[col] = feat_le.transform(dataset_pred[col].astype(str))
+
+
+#this function takes a string column name and returns a list
+#containing indices of dataframe that have outliers in that column
+#Refer: https://towardsdatascience.com/5-ways-to-detect-outliers-that-every-data-scientist-should-know-python-code-70a54335a623
+def OutlierByColumn(colname):
+    rows = dataset_test.shape[0]
+    col_std = np.std(dataset_test[colname])
+    col_mean = np.mean(dataset_test[colname])
+    anomaly_cut_off = col_std * 3
+    lower_limit  = col_mean - anomaly_cut_off 
+    upper_limit = col_mean + anomaly_cut_off
+    anomalies_indices = []
+    for i in range(rows):
+        ele = dataset_test[colname][i]
+        if ele > upper_limit or ele < lower_limit:
+            anomalies_indices.append(i)
+    return anomalies_indices
+
+arr1 = OutlierByColumn('Work_Experience_in_Current_Job')
+arr2 = OutlierByColumn('Age')
+#arr3 = OutlierByColumn('Body_Height_[cm]')
+arr4 = OutlierByColumn('Yearly_Income_in_addition_to_Salary_e.g._Rental_Income')
+arr5 = OutlierByColumn('Size_of_City')
+
+#Union of all lists 
+union_index = np.union1d(arr1,arr2)
+#union_index = np.union1d(union_index,arr3)
+union_index = np.union1d(union_index,arr4)
+union_index = np.union1d(union_index,arr5)
+len(union_index)
+dataset_test = dataset_test.drop(union_index)
+
+y = dataset_test['Total_Yearly_Income_[EUR]'].values
+dataset_test.drop('Total_Yearly_Income_[EUR]', axis=1, inplace=True)
+X = dataset_test[dataset_test.columns].values
+#split 80% of the data to the training set while 20% of the data to test set.
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
+
+X_train = X
+y_train = y
+dataset_pred.drop('Total_Yearly_Income_[EUR]', axis=1, inplace=True)
+X_test = dataset_pred[dataset_pred.columns].values
+
+regressor= RandomForestRegressor(n_estimators=100,random_state=42)
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+#print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))  
+#print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))  
+#print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+store_data['Total Yearly Income [EUR]'] = y_pred
+store_data.to_csv('output.csv', sep=',')
